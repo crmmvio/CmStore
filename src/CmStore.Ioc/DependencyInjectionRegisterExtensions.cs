2827cefce44fab45f076b462e8d2a2bf6f9ec9bb
Cleber Movio <cleber@otimizah.com.br>
2025-04-08 21:02:55 -0300

    Ajuste teste do readme

---
+++ src/CmStore.Ioc/DependencyInjectionRegisterExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CmStore.Ioc
+{
+    public static class DependencyInjectionRegisterExtensions
+    {
+        public static void AddRegisterServicesBuilder(this WebApplicationBuilder builder)
+        {
+
+        }
+    }
+}
